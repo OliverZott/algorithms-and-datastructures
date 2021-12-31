3d8c6ff9c85d3be53ca25687e825aa32562da1b4
OliverZott <zott_oliver@web.de>
2021-12-31 08:50:26 +0100

    Recursion examples added

--- challenges_java/src/main/java/ch03_recursion/IntroductionExamples.java
+++ challenges_java/src/main/java/ch03_recursion/IntroductionExamples.java
@@ -3,21 +3,34 @@ package ch03_recursion;
 public class IntroductionExamples {
 
     public static int factorial(int value) {
-        if (value <= 1) {
-            return 1;
-        } else {
-            return value * factorial(value - 1);
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must non-negative.");
         }
+        if (value == 0 || value == 1) {
+            return 1;
+        }
+        return value * factorial(value - 1);
     }
 
     public static int sum(int value) {
-        if (value < 1) {
-            return 0;
-        } else {
-            return value + sum(value - 1);
+        if (value < 0) {
+            throw new IllegalArgumentException("Value must non-negative.");
         }
+        if (value == 1) {
+            return 1;
+        }
+        return value + sum(value - 1);
     }
 
+    public static int fib(int value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("Value must be greater 0");
+        }
+        if (value == 1 || value == 2) {
+            return 1;
+        }
+        return fib(value-2) + fib(value-1);
+    }
 
     public static void main(String[] args) {
 
--- challenges_java/src/test/java/ch03_recursion/IntroductionExamplesTests.java
+++ challenges_java/src/test/java/ch03_recursion/IntroductionExamplesTests.java
@@ -2,23 +2,50 @@ package ch03_recursion;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static ch03_recursion.IntroductionExamples.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class IntroductionExamplesTests {
 
     @ParameterizedTest
-    @CsvSource({"2, 2", "3, 6", "8, 40320", "-24, 1", "0, 1"})
+    @CsvSource({"2, 2", "3, 6", "8, 40320", "0, 1"})
     public void factorialTest(int value, int expected) {
         var result = factorial(value);
         assertEquals(result, expected);
     }
 
     @ParameterizedTest
-    @CsvSource({"2, 3", "3, 6", "-1, 0", "11, 66", "0, 0"})
-    public void sumTest(int value, int expected) {
+    @ValueSource(ints = {-12})
+    public void factorialTest_invalidInput_throwsException(int value) {
+        assertThrowsExactly(IllegalArgumentException.class, () -> sum(value));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2, 3", "3, 6", "11, 66"})
+    public void sumTest_returnsValidOutput(int value, int expected) {
         var result = sum(value);
         assertEquals(result, expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    public void sumTest_invalidInput_throwsException(int value) {
+        assertThrowsExactly(IllegalArgumentException.class, () -> sum(value));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 1", "4, 3", "8, 21"})
+    public void fib_returnsValidOutput(int value, int expected) {
+        var result = fib(value);
+        assertEquals(result, expected);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    public void fibTest_invalidInput_throwsException(int value) {
+        assertThrowsExactly(IllegalArgumentException.class, () -> sum(value));
     }
 }
