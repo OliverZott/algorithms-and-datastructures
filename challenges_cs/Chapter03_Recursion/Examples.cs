1fd662a46ea0672332eb6301ae360c30a4a8ae7d
Oliver Zott <zott_oliver@web.de>
2023-01-16 07:25:52 +0100

    Recursion examples - digits

--- challenges_cs/Chapter03_Recursion/Examples.cs
+++ challenges_cs/Chapter03_Recursion/Examples.cs
@@ -4,10 +4,47 @@ public static class Examples
 {
     public static int CalcDigits(int value)
     {
+        switch (value)
+        {
+            case < 0:
+                throw new ArgumentException("Value has to be non-negative integer.");
+            case < 10:
+                return 1;
+            default:
+            {
+                var remainder = value / 10;
+                return 1 + CalcDigits(remainder);
+            }
+        }
+    }
+
+    public static int CalcSumOfDigits(int value)
+    {
         if (value == 0) return 0;
 
         var remainder = value / 10;
         var digit = value % 10;
-        return digit + CalcDigits(remainder);
+        return digit + CalcSumOfDigits(remainder);
+    }
+
+    public static int GreatestCommonDivisor(int value1, int value2)
+    {
+        if (value2 == 0) return value1;
+        var value2New = value1 % value2;
+        return GreatestCommonDivisor(value2, value2New);
+    }
+
+    public static int GreatestCommonDivisorIterative(int value1, int value2)
+    {
+        if (value2 == 0) return value1;
+
+        while (value2 != 0)
+        {
+            var value1Temp = value1;
+            value1 = value2;
+            value2 = value1Temp % value2;
+        }
+
+        return value1;
     }
 }
--- challenges_cs/Chapter03_RecursionTests/ExamplesTests.cs
+++ challenges_cs/Chapter03_RecursionTests/ExamplesTests.cs
@@ -5,9 +5,38 @@ namespace Chapter03_RecursionTests;
 public class ExamplesTests
 {
     [Fact]
-    public void CalcDigits()
+    public void CalcSumOfDigitsTest()
     {
-        Assert.Equal(6, Examples.CalcDigits(123));
-        Assert.Equal(6, Examples.CalcDigits(123));
+        Assert.Equal(6, Examples.CalcSumOfDigits(123));
+        Assert.Equal(6, Examples.CalcSumOfDigits(123));
+    }
+
+    [Fact]
+    public void CalDigitsTest()
+    {
+        Assert.Throws<ArgumentException>(() => Examples.CalcDigits(-2));
+        Assert.Equal(1, Examples.CalcDigits(0));
+        Assert.Equal(1, Examples.CalcDigits(9));
+        Assert.Equal(6, Examples.CalcDigits(235808));
+    }
+
+    [Fact]
+    public void GreatestCommonDivisorTest()
+    {
+        Assert.Equal(7, Examples.GreatestCommonDivisor(42, 7));
+        Assert.Equal(1, Examples.GreatestCommonDivisor(17, 2));
+        Assert.Equal(7, Examples.GreatestCommonDivisor(7, 42));
+        Assert.Equal(14, Examples.GreatestCommonDivisor(42, 28));
+        Assert.Equal(14, Examples.GreatestCommonDivisor(42, 14));
+    }
+
+    [Fact]
+    public void GreatestCommonDivisorIterativeTest()
+    {
+        Assert.Equal(7, Examples.GreatestCommonDivisorIterative(42, 7));
+        Assert.Equal(1, Examples.GreatestCommonDivisorIterative(17, 2));
+        Assert.Equal(7, Examples.GreatestCommonDivisorIterative(7, 42));
+        Assert.Equal(14, Examples.GreatestCommonDivisorIterative(42, 28));
+        Assert.Equal(14, Examples.GreatestCommonDivisorIterative(42, 14));
     }
 }
